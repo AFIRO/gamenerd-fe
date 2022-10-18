896aa0dd73d3d27527e09341e91bde29892139b7
Andreeas FIroiu <andrfiro@gmail.com>
2022-10-18 17:12:55 +0200

    eventhandler binding

--- game-nerd/src/components/ToDoItem.tsx
+++ game-nerd/src/components/ToDoItem.tsx
@@ -6,11 +6,29 @@ interface Props {
   done: boolean
 }
 
-export class ToDoItem extends React.Component<Props>{
+interface State {
+  done: boolean
+}
+
+export class ToDoItem extends React.Component<Props, State>{
+  constructor(props: Props) {
+    //super
+    super(props);
+    //state
+    this.state = {
+      done: this.props.done
+    }
+    //event handlers
+    this.onClicked = this.onClicked.bind(this)
+  }
+
   render() {
     return (<li className="list-group-item">
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" name="checkbox" checked={this.props.done} />
+        <input className="form-check-input"
+          type="checkbox" name="checkbox"
+          checked={this.state.done}
+          onClick={this.onClicked} />
         <label className="form-check-label">
           <strong> {this.props.name} </strong><br />
           {this.props.subtext}
@@ -18,5 +36,9 @@ export class ToDoItem extends React.Component<Props>{
       </div>
     </li>
     )
+  }
+
+  onClicked(event: React.MouseEvent) {
+    this.setState({ done: !this.state.done })
   }
 }
