98ae036fe795e6ef1bb329e5872970d8a7214a00
Andreeas FIroiu <andrfiro@gmail.com>
2022-10-31 20:45:56 +0100

    Regels voor verschijnen van knop

--- game-nerd/src/components/news/news.list.item.component.tsx
+++ game-nerd/src/components/news/news.list.item.component.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { News } from "../../api/news/model/news.model";
+import { User } from "../../api/user/model/user.model";
 import { useSession } from "../../contexts/AuthProvider";
 
 export default function NewsListItemComponent(props: News) {
-  const {hasRoles}: { hasRoles: string[] } = useSession();
+  const {hasRoles, user}: { hasRoles: string[], user: User } = useSession();
 
   return (
       <tr>
@@ -11,7 +12,7 @@ export default function NewsListItemComponent(props: News) {
         <td>{props.writer.name}</td>
         <td>{props.content.slice(0, 15).concat("...")}</td>
         <td><Link to={`/news/${props.id}`}><button type="button" className="btn btn-secondary">Link</button></Link></td>
-        {hasRoles.includes("WRITER")? <td><Link to={`/news/update/${props.id}`}><button type="button" className="btn btn-warning">EDIT</button></Link></td>: null}
+        {hasRoles.includes("WRITER") ? props.writer.id === user.id? <td><Link to={`/news/update/${props.id}`}><button type="button" className="btn btn-warning">EDIT</button></Link></td>: <td></td>: null}
         {hasRoles.includes("ADMIN")? <td><Link to={`/news/delete/${props.id}`}><button type="button" className="btn btn-danger">DELETE</button></Link></td>: null}
       </tr>
   )
--- game-nerd/src/components/review/review.list.item.component.tsx
+++ game-nerd/src/components/review/review.list.item.component.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { Review } from "../../api/review/model/review.model";
+import { User } from "../../api/user/model/user.model";
 import { useSession } from "../../contexts/AuthProvider";
 
 export default function ReviewListItemComponent(props: Review) {
-  const {hasRoles}: { hasRoles: string[] } = useSession();
+  const {hasRoles, user}: { hasRoles: string[], user: User } = useSession();
 
   return (
       <tr>
@@ -12,7 +13,7 @@ export default function ReviewListItemComponent(props: Review) {
         <td>{props.content.slice(0, 15).concat("...")}</td>
         <td>{props.score}/10</td>
         <td><Link to={`/reviews/${props.id}`}><button type="button" className="btn btn-secondary">Link</button></Link></td>
-        {hasRoles.includes("WRITER")? <td><Link to={`/reviews/update/${props.id}`}><button type="button" className="btn btn-warning">EDIT</button></Link></td>: null}
+        {hasRoles.includes("WRITER") ? props.writer.id === user.id? <td><Link to={`/reviews/update/${props.id}`}><button type="button" className="btn btn-warning">EDIT</button></Link></td>: <td></td>: null}
         {hasRoles.includes("ADMIN")? <td><Link to={`/reviews/delete/${props.id}`}><button type="button" className="btn btn-danger">DELETE</button></Link></td>: null}
       </tr>
   )
